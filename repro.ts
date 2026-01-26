e9056300ac78c225b68b7e54f561ab352045b06d
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2026-01-26 14:12:29 +0000

    format

--- repro.ts
+++
@@ -1,9 +0,0 @@
-import { defineCommand } from './packages/core/src/lib/command';
-import { z } from 'zod';
-
-export const command = defineCommand({
-  label: 'Deploy Production',
-  context: {
-    env: 'prod',
-  }
-});
